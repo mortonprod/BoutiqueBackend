ba82ddc564c5179201335b81dd8ec76ffe4043cd
Alexander Morton <mortonprod@gmail.com>
2017-06-24 22:52:32 +0100

    Add server and package.json

---
+++ server.js
@@ -0,0 +1,13 @@
+﻿const express = require("express");
+const path = require('path');
+const app = express();
+
+app.set("port", process.env.PORT || 3001);
+
+
+app.use(express.static("client/build"));
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+})
+app.listen(app.get("port"), () => {});
