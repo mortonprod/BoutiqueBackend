219a02dd025bb58fd6ca767e7f2ac62b2946dbbd
Alexander Morton <mortonprod@gmail.com>
2017-06-25 00:09:04 +0100

    Update server to return index for all routes
    -----------
    
    Must update to return the correct html for each route for web crawlers

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.set("port", process.env.PORT || 3001);
 
 app.use(express.static("client/build"));
 
-app.get('/', function (req, res) {
+app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname+'/client/build/index.html'));
 })
 app.listen(app.get("port"), () => {});
