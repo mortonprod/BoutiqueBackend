e5266796f099128c478dc84edc5b62ca0258711a
Alexander Morton <mortonprod@gmail.com>
2017-06-27 22:34:32 +0100

    Add categories to server

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ app.use(express.static("client/build"));
 
 var accountsCollection = null; 
 var productsCollection = null; 
+var categoriesCollection = null; 
 //Hostname(db) comes from service name provide in docker.compose.
 MongoClient.connect("mongodb://db:27017", function(err, db) {
   if(!err) {
@@ -29,6 +30,13 @@ MongoClient.connect("mongodb://db:27017", function(err, db) {
         if(!err){
             console.log("Accessed account products");
             productsCollection = collection
+
+        }
+    });
+    db.collection('categories', function(err, collection) {
+        if(!err){
+            console.log("Accessed account categores");
+            categoriesCollection = collection
 
         }
     });
@@ -57,7 +65,7 @@ app.get('/account',function(req,res){
 
 app.get('/products',function(req,res){
     if(productsCollection != null){
-        var prod = productsCollection.find({}).toArray(function(err,items){
+        productsCollection.find({}).toArray(function(err,items){
             for (let i = 0 ; i < items.length; i++){
                 console.log("Product: " + i + "  " + JSON.stringify(items[i]));
             }
@@ -80,6 +88,36 @@ app.post('/product',upload.single('productFile'), function(req,res,next){
     }
     res.redirect('/account');
 });
+
+app.get('/categories',function(req,res){
+    console.log("Get categories");
+    if(categoriesCollection != null){
+        categoriesCollection.find({}).toArray(function(err,items){
+            let cat = [];
+            for (let i = 0 ; i < items.length; i++){
+                cat.push(items[i]["productCategory"]);
+                console.log("Category: " + i + "  " + JSON.stringify(items[i]["productCategory"]));
+            }
+            res.send(cat);
+        });
+    }else{
+        console.log("Failed to get products");
+    }
+});
+
+
+app.post('/category', function(req,res,next){
+    console.log('\n uploaded name %s',  JSON.stringify(req.body));
+    if(categoriesCollection != null){
+        categoriesCollection.insert(req.body, {w:1}, function(err, result) {
+        console.log("Category added. Result: " +  JSON.stringify(result));
+        });
+    }else{
+        console.log("Failed to add product info.");
+    }
+    res.redirect('/account');
+});
+
 
 app.post('/account',function(req,res){
     console.log("About to add account info: " + req.body);
